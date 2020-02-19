dcc8e3cabaa71a40ac41837cb3353496c472c460
CaelmBleidd <alex.menshutin99@gmail.com>
2020-02-19 18:16:01 +0300

    HW0 Task1

---
+++ hw0/src/Task1.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE TypeOperators #-}
+
+module Task1
+  ( associator
+  , distributivity
+  , eitherAssoc
+  ) where
+
+distributivity :: Either a (b, c) -> (Either a b, Either a c)
+distributivity (Left  a     ) = (Left a, Left a)
+distributivity (Right (b, c)) = (Right b, Right c)
+
+associator :: (a, (b, c)) -> ((a, b), c)
+associator (a, (b, c)) = ((a, b), c)
+
+type (<->) a b = (a -> b, b -> a)
+
+eitherAssocForward :: Either a (Either b c) -> Either (Either a b) c
+eitherAssocForward (Left  a        ) = Left (Left a)
+eitherAssocForward (Right (Left  b)) = Left (Right b)
+eitherAssocForward (Right (Right c)) = Right c
+
+eitherAssocBack :: Either (Either a b) c -> Either a (Either b c)
+eitherAssocBack (Left  (Left  a)) = Left a
+eitherAssocBack (Left  (Right b)) = Right (Left b)
+eitherAssocBack (Right c        ) = Right (Right c)
+
+eitherAssoc :: Either a (Either b c) <-> Either (Either a b) c
+eitherAssoc = (eitherAssocForward, eitherAssocBack)
