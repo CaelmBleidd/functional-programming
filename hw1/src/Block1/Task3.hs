4f14a25c1bd4bd80b5914714ea9817d3a4908c38
CaelmBleidd <alex.menshutin99@gmail.com>
2020-03-26 23:39:37 +0300

    Fix warning

--- hw1/src/Block1/Task3.hs
+++ hw1/src/Block1/Task3.hs
@@ -92,3 +92,16 @@ getTheSmallestValue (Branch _ left _   ) = getTheSmallestValue left
 toList :: BinarySearchTree a -> [a]
 toList Leaf = []
 toList (Branch (x :| xs) left right) = (toList left) ++ (x:xs) ++ (toList right)
+
+-- | Implementation of Foldable for BinarySearchTree
+instance Foldable BinarySearchTree where
+  foldMap :: Monoid m => (a -> m) -> BinarySearchTree a -> m
+  foldMap _ Leaf = mempty
+  foldMap function (Branch x left right) =
+    foldMap function left
+      <> foldMap function x
+      <> foldMap function right
+  foldr :: (a -> b -> b) -> b -> BinarySearchTree a -> b
+  foldr _ z Leaf = z
+  foldr function z (Branch x left right) =
+    foldr function (foldr function (foldr function z right) x) left
--- hw1/src/Block2/Task1.hs
+++ hw1/src/Block2/Task1.hs
@@ -3,17 +3,4 @@ module Block2.Task1
   ()
 where
 
-import           Block1.Task3                   ( BinarySearchTree(..) )
-
--- | Implementation of Foldable for BinarySearchTree
-instance Foldable BinarySearchTree where
-  foldMap :: Monoid m => (a -> m) -> BinarySearchTree a -> m
-  foldMap _ Leaf = mempty
-  foldMap function (Branch x left right) =
-    foldMap function left
-      <> foldMap function x
-      <> foldMap function right
-  foldr :: (a -> b -> b) -> b -> BinarySearchTree a -> b
-  foldr _ z Leaf = z
-  foldr function z (Branch x left right) =
-    foldr function (foldr function (foldr function z right) x) left
+-- | moved to Block1.Task3 because of warnings :(
